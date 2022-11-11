fd2e52776506175f948893913b59980dd5d2a35d
Pappyjay23 <pjinadu02@gmail.com>
2022-11-11 23:18:27 +0100

    Created herosection

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
 			<Router>
 				<Navbar />
 				<Routes>
-					{/* <Route path='/' element={<Home />} /> */}
+					<Route path='/' element={<Home />} />
 				</Routes>
 			</Router>
 		</>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import HeroSection from "../sections/HeroSection";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+	return (
+		<>
+			<HeroSection />
+		</>
+	);
+};
 
-export default Home
+export default Home;
