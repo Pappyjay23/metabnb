cb872fb0bfed62292da8edd014ba8690ee3a7e17
Pappyjay23 <pjinadu02@gmail.com>
2022-11-12 00:05:13 +0100

    Created call to action section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../sections/HeroSection";
 import Inspirations from "../sections/Inspirations";
+import CTA from '../sections/CTA'
 
 const Home = () => {
 	return (
 		<>
 			<HeroSection />
       <Inspirations />
+      <CTA />
 		</>
 	);
 };
